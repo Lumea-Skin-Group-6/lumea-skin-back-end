6523e975d8a07729c9df8f4f01f60a9333406360
TranVanHieu1 <123tranhieu123@gmail.com>
2025-02-11 12:23:18 +0700

    push code CRUD Shift

---
+++ Repository/ShiftRepository.cs
@@ -0,0 +1,61 @@
+﻿using BusinessObject;
+using DAL.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class ShiftRepository : IShiftRepository
+    {
+
+        private readonly AppDbContext _context;
+
+        public ShiftRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void AddAsync(Shift shift)
+        {
+            _context.Shifts.Add(shift);
+            _context.SaveChangesAsync();
+        }
+
+        public void DeleteAsync(int id)
+        {
+            Shift shift =  GetShiftById(id);
+            if (shift != null)
+            {
+                _context.Shifts.Remove(shift);
+                _context.SaveChangesAsync();
+            }
+        }
+
+        public List<Shift> GetAllShift()
+        {
+            return _context.Shifts.ToList();
+        }
+
+        public Shift GetShiftById(int id)
+        {
+            return _context.Shifts.Find(id);
+        }
+
+        public void UpdateAsync(Shift shift)
+        {
+            Shift exisShift = GetShiftById(shift.Id);
+            if (shift != null)
+            {
+                exisShift.Date = shift.Date;
+                exisShift.StartTime = shift.StartTime;
+                exisShift.EndTime = shift.EndTime;
+                exisShift.MaxStaff = shift.MaxStaff;
+                exisShift.MaxStaff = shift.MaxStaff;
+                exisShift.Status = shift.Status;
+                _context.SaveChangesAsync();
+            }
+        }
+    }
+}
