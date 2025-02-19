0d3c8de5bb3dc8fb987911b6ef9c85a060b6b58e
Styematic <128201525+Taikt08s@users.noreply.github.com>
2025-02-19 13:32:03 +0700

    Update Program.cs

--- SkincareBookingApp/Program.cs
+++ SkincareBookingApp/Program.cs
@@ -154,7 +154,7 @@ var app = builder.Build();
 app.UseExceptionHandler(_ => { });
 
 app.UseRouting();
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
