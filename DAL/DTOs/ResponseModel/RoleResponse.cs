5eb8d8681e71266860ae94383e0e422c6ec44cad
TranVanHieu1 <123tranhieu123@gmail.com>
2025-03-12 13:07:04 +0700

    get all role

---
+++ DAL/DTOs/ResponseModel/RoleResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DTOs.ResponseModel
+{
+    public class RoleResponse
+    {
+        public int RoleID { get; set; }
+
+        public string RoleName { get; set; }
+    }
+}
---
+++ SkincareBookingApp/Controllers/RoleController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Service.Interfaces;
+using Service.Services;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace SkincareBookingApp.Controllers
+{
+    [Route("api/roles")]
+    [ApiController]
+    [ApiExplorerSettings(GroupName = "Roles")]
+    public class RoleController : ControllerBase
+    {
+        private readonly IRoleService roleService;
+
+        public RoleController(IRoleService roleService)
+        {
+            this.roleService = roleService;
+        }
+
+        [HttpGet]
+        [SwaggerOperation(Summary = "get all Roles")]
+        public IActionResult GetAllRole()
+        {
+            var role = roleService.GetAllRole();
+            return Ok(role);
+        }
+    }
+}
