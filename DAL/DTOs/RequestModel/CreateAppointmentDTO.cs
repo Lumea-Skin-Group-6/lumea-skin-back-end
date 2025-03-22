a5a5f9b580d8341a572b300d1a7e30d92ed9d2c9
BuiDuyKhang2003 <huvo201223@gmail.com>
2025-03-22 15:20:17 +0700

    crud appointment

---
+++ DAL/DTOs/RequestModel/CreateAppointmentDTO.cs
@@ -0,0 +1,33 @@
+﻿using BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DTOs.RequestModel
+{
+    public class CreateAppointmentDTO
+    {
+        public int AccountId { get; set; }
+        public decimal Amount { get; set; }
+        public string? Note { get; set; }
+        public List<AppointmentDetailDTO> Services { get; set; }
+    }
+
+    public class AppointmentDetailDTO
+    {
+        public int ServiceId { get; set; }
+        public int TherapistId { get; set; }
+        public decimal Price { get; set; }
+        public ServiceType Type { get; set; }
+        public TimeSpan Duration { get; set; }
+        public List<AppointmentDetailDateDTO> AppointmentDates { get; set; }
+    }
+
+    public class AppointmentDetailDateDTO
+    {
+        public DateTime Date { get; set; }
+    }
+}
---
+++ SkincareBookingApp/Controllers/AppointmentController.cs
@@ -0,0 +1,50 @@
+﻿using DAL.DTOs.RequestModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service.Interfaces;
+using SkincareBookingApp.Helpers;
+using System.Net;
+
+namespace SkincareBookingApp.Controllers
+{
+    [Route("api/appointments")]
+    [ApiController]
+    [ApiExplorerSettings(GroupName = "Appointments")]
+    public class AppointmentController : ControllerBase
+    {
+        private readonly IAppointmentService _appointmentService;
+
+        public AppointmentController(IAppointmentService appointmentService)
+        {
+            _appointmentService = appointmentService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAppointment([FromBody] CreateAppointmentDTO appointmentDTO)
+        {
+            var result = await _appointmentService.CreateAppointmentAsync(appointmentDTO);
+            return result != null
+                ? CustomSuccessHandler.ResponseBuilder(HttpStatusCode.Created, "Appointment added successfully.", result)
+                : BadRequest(new { message = "Failed to add appointment." });
+        }
+
+        //[HttpGet("{id}")]
+        //public async Task<IActionResult> GetAppointmentById(int id)
+        //{
+        //    var appointment = await _appointmentService.GetAppointmentByIdAsync(id);
+        //    if (appointment == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return Ok(appointment);
+        //}
+
+        //[HttpGet("account/{accountId}")]
+        //public async Task<IActionResult> GetAppointmentsByAccount(int accountId)
+        //{
+        //    var appointments = await _appointmentService.GetAppointmentsByAccountIdAsync(accountId);
+        //    return Ok(appointments);
+        //}
+    }
+
+}
